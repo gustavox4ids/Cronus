32fdd51192a4424b8d7fdc863de24d71fb2aec5c
Carlos Heitor Lain <atendimento@virtuati.com.br>
2012-11-08 00:17:51 -0200

    Definição de diferenças nos leveis para dividir experiência em um grupo são configurados agora em /conf/battle/party.conf O level máximo 5000 já é aceito pelo emulador por padrão, defina o level máximo do servidor em conf/battle/client.conf

--- src/map/map.h
+++ src/map/map.h
@@ -39,7 +39,7 @@ enum E_MAPSERVER_ST
 #define NATURAL_HEAL_INTERVAL 500
 #define MIN_FLOORITEM 2
 #define MAX_FLOORITEM START_ACCOUNT_NUM
-#define MAX_LEVEL 150
+#define MAX_LEVEL 5000 // Máximo aceito pelo emulador, assim sendo definido em /conf/battle/client.conf [Raizen]
 #define MAX_DROP_PER_MAP 48
 #define MAX_IGNORE_LIST 20 // official is 14
 #define MAX_VENDING 12
